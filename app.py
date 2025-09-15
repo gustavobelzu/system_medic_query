6b0d4ce4a20b6358f3ccc504f151142855a06679
Gustavo Belzu <gustavo.belzu@sistemas.edu.bo>
2025-09-15 13:49:08 -0400

    modifico app.py

--- app.py
+++ app.py
@@ -1,4 +1,32 @@
-from menus import menu_principal
+from modulo_paciente import menu_pacientes
+from modulo_usuario import menu_usuarios
+from modulo_ingreso import menu_ingresos
+from modulo_egreso import menu_egresos
+
+def menu_principal():
+    while True:
+        print("\n=== SISTEMA DE CONTROL DE EMERGENCIAS - CLÍNICA LA FUENTE ===")
+        print("1. Gestión de Pacientes")
+        print("2. Gestión de Usuarios")
+        print("3. Gestión de Ingresos")
+        print("4. Gestión de Egresos")
+        print("0. Salir")
+
+        opcion = input("Seleccione una opción: ").strip()
+
+        if opcion == "1":
+            menu_pacientes()
+        elif opcion == "2":
+            menu_usuarios()
+        elif opcion == "3":
+            menu_ingresos()
+        elif opcion == "4":
+            menu_egresos()
+        elif opcion == "0":
+            print("👋 Saliendo del sistema...")
+            break
+        else:
+            print("❌ Opción inválida. Intente nuevamente.")
 
 if __name__ == "__main__":
     menu_principal()
