340527e2464ecc5ab08f689e48cec443fd45c797
Gustavo Belzu <gustavo.belzu@sistemas.edu.bo>
2025-10-26 01:44:01 -0400

    creando dashboard window

---
+++ desktop_gui/dashboard_window.py
@@ -0,0 +1,43 @@
+from PySide6.QtWidgets import QMainWindow, QMessageBox
+from PySide6.QtUiTools import QUiLoader
+from PySide6.QtCore import QFile
+import sys
+import os
+
+# Agregar tus módulos al path
+sys.path.append(os.path.join(os.path.dirname(__file__), "..", "modules"))
+
+from estados import menu_estados
+from pacientes import menu_pacientes
+from usuarios import menu_usuarios
+from ingresos import menu_ingresos
+from egresos import menu_egresos
+from reportes import menu_reportes
+
+class DashboardWindow(QMainWindow):
+    def __init__(self, user):
+        super().__init__()
+        loader = QUiLoader()
+        ui_file = QFile("dashboard.ui")
+        ui_file.open(QFile.ReadOnly)
+        self.ui = loader.load(ui_file, self)
+        ui_file.close()
+
+        self.user = user
+        self.ui.lbl_user.setText(f"{user[1]} ({user[2]})")
+
+        # Conectar botones
+        self.ui.btn_estados.clicked.connect(lambda: self.abrir_modulo(menu_estados))
+        self.ui.btn_pacientes.clicked.connect(lambda: self.abrir_modulo(menu_pacientes))
+        self.ui.btn_usuarios.clicked.connect(lambda: self.abrir_modulo(menu_usuarios))
+        self.ui.btn_ingresos.clicked.connect(lambda: self.abrir_modulo(menu_ingresos))
+        self.ui.btn_egresos.clicked.connect(lambda: self.abrir_modulo(menu_egresos))
+        self.ui.btn_reportes.clicked.connect(lambda: self.abrir_modulo(menu_reportes))
+        self.ui.btn_salir.clicked.connect(self.close)
+
+    def abrir_modulo(self, funcion):
+        try:
+            funcion(self.user)
+        except TypeError:
+            funcion()
+        QMessageBox.information(self, "Módulo cerrado", "Regresando al panel principal.")
